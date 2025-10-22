189a921ade83ac1334b26ca2065ff5ea824878ac
quangphucthan <quangphucthan@gmail.com>
2025-10-22 11:20:14 -0300

    adding fork logic

--- Shell 4 Linux/main.c
+++ Shell 4 Linux/main.c
@@ -3,7 +3,35 @@
 #include <unistd.h>
 #include <sys/wait.h>
 
-int main()
-{
-    
+#define MAX_LINES 80
+
+int main() {
+    char *args[MAX_LINES / 2 + 1];
+    int should_run = 1;
+    int wait_status;
+
+    while (should_run) {
+        printf("CSCI3120>");
+        fflush(stdout);
+
+        pid_t pid = fork();
+
+        if (pid < 0) {
+            fprintf(stderr, "Fork failed\n");
+            exit(1);
+        }
+        else if (pid == 0) {
+            if (fgets(args[0], sizeof(args[0]), stdin) == NULL) {
+                exit(0);
+            }
+
+            
+        }
+        else {
+            wait(&wait_status);
+            printf("Child process completed\n");
+        }
+    }
+
+    return 0;
 }
