42f3fd972fe5a64313238216d5de06b42466bfce
quangphucthan <quangphucthan@gmail.com>
2025-12-02 00:20:09 -0400

    feat: added file opening

--- CPU Scheduling/main.c
+++ CPU Scheduling/main.c
@@ -2,5 +2,13 @@
 #include <stdlib.h>
 
 int main() {
-    
+    FILE *file;
+    file = fopen("TaskSpec.txt", "r");
+
+    if (file == NULL) {
+        perror("Error opening file");
+        return EXIT_FAILURE;
+    }
+
+    fclose(file);
 }
