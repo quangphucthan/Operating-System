4d919af9a3304c7cf577b6bf23d25fa422a9784e
quangphucthan <quangphucthan@gmail.com>
2025-12-03 13:10:53 -0400

    feat: printing data

--- CPU Scheduling/main.c
+++ CPU Scheduling/main.c
@@ -3,12 +3,13 @@
 #include <string.h>
 
 #define MAX 50
+#define TIME_QUANTUM 4
+void FCFS(char tasks[][3], int arrival[], int burst[], int count);
 
-void main() {
+int main() {
     char tasks[MAX][3];
     int arrival[MAX];
     int burst[MAX];
-    int timeQuantum = 4;
     char buffer[100];
     int count = 0;
 
@@ -17,16 +18,32 @@ void main() {
 
     if (file == NULL) {
         perror("Error opening file");
+        return EXIT_FAILURE;
     }
 
+    // Read tasks, arrival times, and burst times from file
     while (fgets(buffer, sizeof(buffer), file)) {
         sscanf(buffer, "%[^,],%d,%d", tasks[count], &arrival[count], &burst[count]);
         count++;
     }
 
-    for (int j = 0; j < count; j++) {
-        printf("Task: %s, Arrival: %d, Burst: %d\n", tasks[j], arrival[j], burst[j]);
-    }
+    FCFS(tasks, arrival, burst, count);
 
     fclose(file);
+    return 0;
+}
+
+void FCFS(char tasks[][3], int arrival[], int burst[], int count) {
+    int answers = 0;
+    printf("FCFS:\n");
+
+    for (int i = 0; i < count; i++) {
+        printf("%s\t", tasks[i]);
+        printf("%d\t", arrival[i]);
+        printf("%d\n", burst[i]);
+
+        answers += arrival[i] - i;
+    }
+
+    printf("Wating Time %s: %d\n", tasks[count-1], answers);
 }
