9b1a7f7895296c7fb7c8057bfa078fc4e4f7ca1f
quangphucthan <quangphucthan@gmail.com>
2025-12-03 01:04:57 -0400

    feat: read txt file and parse it into the array

--- CPU Scheduling/main.c
+++ CPU Scheduling/main.c
@@ -1,13 +1,31 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 
-int main() {
+#define MAX 50
+
+void main() {
+    char tasks[MAX][3];
+    int arrival[MAX];
+    int burst[MAX];
+    int timeQuantum = 4;
+    char buffer[100];
+    int count = 0;
+
     FILE *file;
     file = fopen("TaskSpec.txt", "r");
 
     if (file == NULL) {
         perror("Error opening file");
-        return EXIT_FAILURE;
+    }
+
+    while (fgets(buffer, sizeof(buffer), file)) {
+        sscanf(buffer, "%[^,],%d,%d", tasks[count], &arrival[count], &burst[count]);
+        count++;
+    }
+
+    for (int j = 0; j < count; j++) {
+        printf("Task: %s, Arrival: %d, Burst: %d\n", tasks[j], arrival[j], burst[j]);
     }
 
     fclose(file);
